644cbfaa2411fcb71c900d21e49de19414e42008
RWMumo <ritawanza5@gmail.com>
2022-02-03 13:22:05 +0300

    row

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
+
+// hover to show
 $(document).ready(function() {
   $(".designa").hover(function() {
     $(".designashowing").show();
   });
 });
-// console.log(designa)
+
 
 $(document).ready(function() {
   $(".designb").hover(function() {
     $(".designbshowing").show();
   });
 });
-// console.log(designb)
+
 
 $(document).ready(function() {
   $(".designc").hover(function() {
     $(".designcshowing").show();
   });
 });
-// console.log(designc)
+
+// click to hide
+$(document).ready(function() {
+  $(".designa").mouseleave(function() {
+    $(".designashowing").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".designb").mouseleave(function() {
+    $(".designbshowing").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".designc").mouseleave(function() {
+    $(".designcshowing").hide();
+  });
+});
 
 
 
