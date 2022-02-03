3c9955ec9671f829da0a8bbc59090be4e245ab42
RWMumo <ritawanza5@gmail.com>
2022-02-03 03:00:31 +0300

    console log

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ $(document).ready(function() {
     $(".designashowing").show();
   });
 });
+console.log(designa)
 
 $(document).ready(function() {
   $(".designb").hover(function() {
     $(".designbshowing").show();
   });
 });
+console.log(designb)
 
 $(document).ready(function() {
   $(".designc").hover(function() {
     $(".designcshowing").show();
   });
 });
+console.log(designc)
 
